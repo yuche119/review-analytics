c9d384bc1f13e5d8bcf36e74838d60f34d130af0
Castiel Yu <yuche119@gmail.com>
2020-02-28 20:19:56 +0800

    second.py

--- read.py
+++ read.py
@@ -3,8 +3,15 @@ count = 0
 with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
-        count += 1
-        if count % 10000 == 0:
-            print(len(data))
-print(data[0])
-# print(len(data))
+print('總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+    sum_len += len(d)
+    print(sum_len)
+
+print('平均留言', sum_len/len(data))
+#         count += 1
+#         if count % 10000 == 0:
+#             print(len(data))
+# print(data[0])
